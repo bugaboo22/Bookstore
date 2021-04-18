fb2c9f0acec893ef18478c22159a33aeb100c73e
Lauri <lauri.vastela@gmail.com>
2021-04-18 14:54:02 +0300

    final fixes

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -18,25 +18,8 @@ import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class BookstoreApplication {
 	
-	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner Bookdemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository ) {
-		return (args) -> {
-			crepository.deleteAll();
-			crepository.save(new Category("Fantasy"));
-			crepository.save(new Category("Horror"));
-			crepository.save(new Category("WW2"));
-			
-			brepository.deleteAll();
-			brepository.save(new Book("13423-312", "Miika Vähänen", "Miika ihmemaassa", "2020", crepository.findByName("Fantasy").get(0)));
-			brepository.save(new Book("13423-313", "Pekka Pekkanen", "Pekka töpöhäntä", "2020", crepository.findByName("Horror").get(0)));
-			brepository.save(new Book("13423-314", "Pauli Paulinen", "Paulin lihapullat", "2020", crepository.findByName("WW2").get(0)));
-			
-			
-		};
-	}
+	
 }
